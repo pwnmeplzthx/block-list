e2131959c649f0e3b5a9ef72d0097979c295805e
pwnmeplzthx <pshumakov11@mail.ru>
2023-12-21 23:50:35 +0300

    prisma init

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 class HelloWorldDto {
   @ApiProperty()
@@ -11,11 +14,14 @@ class HelloWorldDto {
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @ApiOkResponse({
-      type: HelloWorldDto,
-  })
-    getHello(): HelloWorldDto {
+    @Get()
+    @ApiOkResponse({
+        type: HelloWorldDto,
+    })
+    async getHello(): Promise<HelloWorldDto> {
+        const users = await prisma.user.findMany({});
+        console.log(users);
+
         return {
             message: this.appService.getHello(),
         };
